bbb65e66cf0738fe948090a259c2487c4dcd8b44
German_Pipchenko <chenk.n@ya.ru>
2023-01-21 20:31:37 +0300

    Добавить логику функции database::isExistLogin()

--- DataBase.cpp
+++ DataBase.cpp
@@ -1,9 +1,14 @@
 #include "DataBase.h"
+
 #include <vector>
+#include <assert.h>
+
 
 namespace {
 	std::vector<std::string> logins{ "login_1", "login_2" };
 }
+
+
 
 void database::initialize()
 {
@@ -14,7 +19,14 @@ void database::initialize()
 
 bool database::isExistLogin(std::string login)
 {
-	return false;
+	//Итератор - const! Чтобы нельзя было изменить вектор или данные в нём.
+	std::vector<std::string>::const_iterator it =
+		std::find(logins.begin(), logins.end(), login);	//Для поиска использовать алгоритм STL
+	//Элемент не найден
+	if (it == logins.end()) {
+		return false;
+	}
+	return true;
 }
 
 
@@ -28,5 +40,7 @@ bool database::isCorrectPassword(std::string login, std::string password)
 
 void database::test()
 {
-
+	assert(database::isExistLogin("login_1") == true);
+	assert(database::isExistLogin("login_2") == true);
+	assert(database::isExistLogin("not_exist") == false);
 }
