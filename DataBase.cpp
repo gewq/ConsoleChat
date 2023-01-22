aaf5525c01274779e5606f37cc4327ebcbe143f5
German_Pipchenko <chenk.n@ya.ru>
2023-01-22 15:26:28 +0300

    Заменить в базе данных string на User

--- DataBase.cpp
+++ DataBase.cpp
@@ -3,9 +3,11 @@
 #include <vector>
 #include <assert.h>
 
+#include "User.h"
 
 namespace {
-	std::vector<std::string> logins{ "login_1", "login_2" };
+	//Зарегистрированные пользователи
+	std::vector<User> users;
 }
 
 
@@ -19,11 +21,13 @@ void database::initialize()
 
 bool database::isExistLogin(const std::string& login)
 {
+	//Ищем объект класса User
+	User wanted("1", login, "1");	//При поиске сравниваем только login
 	//Итератор - const! Чтобы нельзя было изменить вектор или данные в нём.
-	std::vector<std::string>::const_iterator it =
-		std::find(logins.begin(), logins.end(), login);	//Для поиска использовать алгоритм STL
+	std::vector<User>::const_iterator it =
+		std::find(users.begin(), users.end(), wanted);	//Используем алгоритм STL (find)
 	//Элемент не найден
-	if (it == logins.end()) {
+	if (it == users.end()) {
 		return false;
 	}
 	return true;
@@ -40,7 +44,14 @@ bool database::isCorrectPassword(const std::string& login, const std::string& pa
 
 void database::test()
 {
+	//Поместить тестовое значение
+	User u1("1", "login_1", "1");
+	users.push_back(u1);
+
 	assert(database::isExistLogin("login_1") == true);
-	assert(database::isExistLogin("login_2") == true);
 	assert(database::isExistLogin("not_exist") == false);
+
+	//Очистить от тестовых значений
+	users.clear();
+	assert(users.empty() == true);
 }
