fc994cf0b0ba1c67c76b1b7ceaad4a4f877be9ef
German_Pipchenko <chenk.n@ya.ru>
2023-01-25 22:01:52 +0300

    Переместить тестирование функций модуля DataBase в отдельные функции

--- DataBase.cpp
+++ DataBase.cpp
@@ -71,32 +71,34 @@ bool database::isCorrectPassword(const std::string& login, const std::string& pa
 }
 
 
+/**
+Запустить тест функции isExistLogin()
+*/
+static void testIsExistLogin();
+
+/**
+Запустить тест функции isExistName()
+*/
+static void testIsExistName();
+
+/**
+Запустить тест функции isCorrectPassword()
+*/
+static void testIsCorrectPassword();
+
+/**
+Запустить тест функции getUserPosition()
+*/
+static void testGetUserPosition();
+
+
 
 void database::test()
 {
-	//Поместить тестовое значение
-	User u1("1", "login_1", "pass_1");
-	User u2("name_2", "login_2", "pass_2");
-	users.push_back(u1);
-	users.push_back(u2);
-
-	assert(database::isExistLogin("login_1") == true);
-	assert(database::isExistLogin("not_exist") == false);
-
-	assert(database::isExistName("name_2") == true);
-	assert(database::isExistName("not_exist") == false);
-
-	assert(getUserPosition("login_1") == 0);
-	assert(getUserPosition("login_2") == 1);
-
-	assert(database::isCorrectPassword("login_1", "pass_1") == true);
-	assert(database::isCorrectPassword("login_2", "pass_2") == true);
-	assert(database::isCorrectPassword("login_1", "passdad") == false);
-	assert(database::isCorrectPassword("login_2", "2") == false);
-
-	//Очистить от тестовых значений
-	users.clear();
-	assert(users.empty() == true);
+	testIsExistLogin();
+	testIsExistName();
+	testIsCorrectPassword();
+	testGetUserPosition();
 }
 
 
@@ -117,4 +119,71 @@ static int8_t getUserPosition(const std::string& login)
 	else {
 		return static_cast<int8_t> (iterator - users.begin());
 	}
+}
+
+
+
+static void testIsExistLogin()
+{
+	//Поместить тестовое значение
+	User user("name", "login", "password");
+	users.push_back(user);
+
+	assert(database::isExistLogin(user.getLogin()) == true);
+	assert(database::isExistLogin("incorrect_login") == false);
+
+	//Очистить от тестовых значений
+	users.clear();
+	assert(users.empty() == true);
+}
+
+
+
+static void testIsExistName()
+{
+	//Поместить тестовое значение
+	User user("name", "login", "password");
+	users.push_back(user);
+
+	assert(database::isExistName(user.getName()) == true);
+	assert(database::isExistName("incorrect_name") == false);
+
+	//Очистить от тестовых значений
+	users.clear();
+	assert(users.empty() == true);
+}
+
+
+
+static void testIsCorrectPassword()
+{
+	//Поместить тестовое значение
+	User user("name", "login", "password");
+	users.push_back(user);
+
+	assert(database::isCorrectPassword(user.getLogin(), user.getPassword()) == true);
+	assert(database::isCorrectPassword(user.getLogin(), "incorrect_password") == false);
+	assert(database::isCorrectPassword("incorrect_login", user.getPassword()) == false);
+
+	//Очистить от тестовых значений
+	users.clear();
+	assert(users.empty() == true);
+}
+
+
+
+static void testGetUserPosition()
+{
+	//Поместить тестовое значение
+	User user_1("name_1", "login_1", "password_1");
+	User user_2("name_2", "login_2", "password_2");
+	users.push_back(user_1);
+	users.push_back(user_2);
+
+	assert(getUserPosition(user_1.getLogin()) == 0);
+	assert(getUserPosition(user_2.getLogin()) == 1);
+
+	//Очистить от тестовых значений
+	users.clear();
+	assert(users.empty() == true);
 }
